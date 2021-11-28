e94959acb51276b806b91269e17d77dfcd548193
mica-reyes <reyessmicaela@gmail.com>
2021-11-28 01:42:25 -0300

    dos

--- PI-Food-main/client/src/components/Form.jsx
+++ PI-Food-main/client/src/components/Form.jsx
@@ -60,21 +60,7 @@ export default function Form() {
 }
 
 
-/* 
-                <select name='diets' value='diets' onChange={selectDiets}>
-                    <option value="gluten free">gluten free</option>
-                    <option value="ketogenic">ketogenic</option>
-                    <option value="vegetarian">vegetarian</option>
-                    <option value="lacto-vegetarian">lacto-vegetarian</option>
-                    <option value="ovo-vegetarian">ovo-vegetarian</option>
-                    <option value="vegan">vegan</option>
-                    <option value="pescetarian">pescetarian</option>
-                    <option value="paleo">paleo</option>
-                    <option value="primal">primal</option>
-                    <option value="low FODMAP">low FODMAP</option>
-                    <option value="whole30">whole30</option>
-                </select>
- */
+
 /* 
 
 [ ] Un formulario controlado con los siguientes campos
